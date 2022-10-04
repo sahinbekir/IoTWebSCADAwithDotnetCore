1c8b1ecb390002e88bdb03c02e67926c67446857
schahinbekir <bsbekirsahinbs@gmail.com>
2022-10-05 00:26:56 +0300

    Done: CORS Add, DB connection, DB create Login Control, User CRUD operations. To-Do: Device, Project, Connection CRUD operations, MQTT operations.

---
+++ IoTWebSCADA_convert_Python_into_DotnetCore/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using IoTWebSCADA_convert_Python_into_DotnetCore.Database;
+using IoTWebSCADA_convert_Python_into_DotnetCore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IoTWebSCADA_convert_Python_into_DotnetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IoTWS_DbContext _IoTWS_DbContext;
+        public LoginController(IoTWS_DbContext iotws_DbContext)
+        {
+            _IoTWS_DbContext = iotws_DbContext;
+        }
+
+        //Search with Name
+        [HttpGet("{login}")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetByTitle(string username, string password)
+        {
+            var user = await _IoTWS_DbContext.Users.SingleOrDefaultAsync(c => c.UserName == username && c.UserPassword == password);
+            return user == null ? NotFound("Böyle Bir Kullanıcı Bulunamadı...") : Ok(user);
+        }
+    }
+}
--- IoTWebSCADA_convert_Python_into_DotnetCore/Controllers/UserController.cs
+++ IoTWebSCADA_convert_Python_into_DotnetCore/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IoTWebSCADA_convert_Python_into_DotnetCore.Database;
+using IoTWebSCADA_convert_Python_into_DotnetCore.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace IoTWebSCADA_convert_Python_into_DotnetCore.Controllers
 {
@@ -7,5 +10,72 @@ namespace IoTWebSCADA_convert_Python_into_DotnetCore.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IoTWS_DbContext _IoTWS_DbContext;
+        public UserController(IoTWS_DbContext iotws_DbContext)
+        {
+            _IoTWS_DbContext = iotws_DbContext;
+        }
+
+        //Read All List
+        [HttpGet]
+        public async Task<IEnumerable<User>> Get()
+
+            => await _IoTWS_DbContext.Users.ToListAsync();
+
+        //Search with Id
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var user = await _IoTWS_DbContext.Users.FindAsync(id);
+            return user == null ? NotFound("Bu Id Değerinde Bir Kullanıcı Yok.") : Ok(user);
+        }
+
+        //Search with Name
+        [HttpGet("search/{title}")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetByTitle(string userfirstname, string userlastname)
+        {
+            var user = await _IoTWS_DbContext.Users.SingleOrDefaultAsync(c => c.UserFirstName==userfirstname && c.UserLastName == userlastname);
+            return user == null ? NotFound("Bu Ad ve Soyad Değerinde Bir Kullanıcı Yok.") : Ok(user);
+        }
+        //User Add
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(User user)
+        {
+            await _IoTWS_DbContext.Users.AddAsync(user);
+            await _IoTWS_DbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = user.UserId }, user);
+        }
+        //User Update
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(int id, User user)
+        {
+            if (id != user.UserId) return BadRequest();
+            _IoTWS_DbContext.Entry(user).State = EntityState.Modified;
+            await _IoTWS_DbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+        //User Delete with Id
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var UserDel = await _IoTWS_DbContext.Users.FindAsync(id);
+            if (UserDel == null) return NotFound();
+
+            _IoTWS_DbContext.Users.Remove(UserDel);
+            await _IoTWS_DbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
